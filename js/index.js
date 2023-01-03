710b8ff8bb7b55b6cc880dc61ddec2dc1f949701
natansw <assaosawa@gmail.com>
2023-01-03 11:00:00 -0300

    ajustes em validate

--- js/index.js
+++ js/index.js
@@ -19,5 +19,25 @@ $(document).ready(function(){
         autoplay: true,
         arrows: false,
         dots: true
+   });
+
+    //  Validação do formulário de contato
+    $('.formulario').validate({
+        rules: {
+            nome: 'required',
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: 'required'
+        },
+        messages: {
+            nome: "Por favor preencha o campo Nome",
+            email: {
+                required: 'Por favor preencha o e-mail.',
+                email: 'Por favor preencha um e-mail válido'
+            },
+            mensagem: "Por favor preencha o campo Mensagem"
+        }
     });
 });
